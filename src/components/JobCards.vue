773e74ef70491d5beba8ae69a8dca65286ce664c
Juan Felipe López <juanfelipelopez55@gmail.com>
2024-03-01 13:05:08 -0500

    little grid problem on publicationDate fixed

--- src/components/JobCards.vue
+++ src/components/JobCards.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="container" style="width: 100%">
-        <ul class="row job-item list-unstyled d-flex justify-content-center" style="" v-for="(jobs, index) in jobsList" :key="index">
+        <ul class="row job-item list-unstyled d-flex justify-content-center" style="" v-for="(job, index) in jobsList" :key="index">
             <div class="card w-100">
                 <div class="card-body p-2 p-md-3">
                     <div class="row align-items-center">
                         <div class="justify-content-center align-items-center col-md-1 col-3">
-                            <img :src="jobs.img" alt="Company logo" width="70" height="70" class="rounded">
+                            <img :src="job.img" alt="Company logo" width="70" height="70" class="rounded">
                         </div>
                         <div class="col-md-11 col-9">
                             <div class="row align-items-center">
                                 <div class="mb-2 col" style="border-radius: 10px;">
                                     <a href="#" class="text-dark stretched-link d-inline d-md-inline-flex" style="text-decoration: none;">
-                                        <h2 class="job-title bold pl-md-2 pr-md-2 mb-0 pb-0 me-2">{{jobs.vacancyName}}</h2>
-                                        <section class="pl-md-2 d-inline d-md-inline-flex align-items-center text-truncate" v-for="(skill, index) in jobs.skills" :key="index">
+                                        <h2 class="job-title bold pl-md-2 pr-md-2 mb-0 pb-0 me-2">{{job.vacancyName}}</h2>
+                                        <section class="pl-md-2 d-inline d-md-inline-flex align-items-center text-truncate" v-for="(skill, index) in job.skills" :key="index">
                                             <div role="button" class="skill-tag text-truncate border border-dark text-dark text-capitalize ms-1" style="font-size: 0.8em; border-radius: 10px;">
                                                 <span class="mx-2">{{ skill }}</span>
                                                 
@@ -29,7 +29,7 @@
                                             <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"></path>
                                         </g>
                                     </svg> <span>
-                                        {{jobs.company}}
+                                        {{job.company}}
                                     </span></div>
                                 <div class="text-truncate col">
                                     <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="geo alt" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-geo-alt b-icon bi">
@@ -38,7 +38,7 @@
                                             <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"></path>
                                         </g>
                                     </svg> <span>
-                                        {{jobs.workModality}}
+                                        {{job.workModality}}
                                     </span></div>
                                 <div class="text-truncate col">
                                     <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="cash" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-cash b-icon bi">
@@ -47,7 +47,7 @@
                                             <path d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2H3z"></path>
                                         </g>
                                     </svg> <span>
-                                        {{jobs.salary}}
+                                        {{job.salary}}
                                     </span></div>
                                 <div class="d-none d-md-inline-block col">
                                     <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="people" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-people b-icon bi">
@@ -55,7 +55,7 @@
                                             <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"></path>
                                         </g>
                                     </svg>
-                                    {{jobs.vacancies}}
+                                    {{job.vacancies}}
                                 </div>
                                 <div class="text-truncate col">
                                     <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="calendar3" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-calendar3 b-icon bi">
@@ -63,7 +63,8 @@
                                             <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"></path>
                                             <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"></path>
                                         </g>
-                                    </svg>{{jobs.publicationDate}}
+                                    </svg>
+                                    <span class="ms-2">{{job.publicationDate}}</span>
                                 </div>
                                 <div class="text-truncate col"></div>
                             </div>
